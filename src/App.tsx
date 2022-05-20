44caa269faa76202db6cebb31b8a92425270d0da
Daniel Fernando Lourusso <dflourusso@gmail.com>
2022-05-20 14:11:01 -0300

    feat: add basic feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function randomIntFromInterval(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const allRocks = Array(75).fill(null).map((_, index) => index + 1)
+
 function App() {
+  const [pickedRocks, setPickedRocks] = useState<number[]>([])
+  const unpicked = allRocks.filter(p => !pickedRocks.includes(p))
+  const lastPicked = pickedRocks.slice(0, 3)
+  const [submitDisabled, setSubmitDisabled] = useState(false)
+
+  const addPickedRock = (rock: number) => {
+    setPickedRocks(p => [rock, ...p])
+  }
+
+  const pickRock = () => {
+    const pickedRock = randomIntFromInterval(0, unpicked.length - 1)
+    addPickedRock(unpicked[pickedRock])
+    setSubmitDisabled(true)
+    setTimeout(() => {
+      setSubmitDisabled(false)
+    }, 2000)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='pick-button' disabled={submitDisabled || unpicked.length === 0} onClick={pickRock}>Sortear nova pedra</button>
+      <div className='last-picked-container'>
+
+        {lastPicked.length > 0 && <p>Últimas pedras sorteadas: </p>}
+        <div className='rocks-table'>
+          {lastPicked.map((rock, index) => {
+            return <div className={index === 0 ? 'last-active' : "active"}>
+              <span key={rock}>{rock}</span>
+            </div>
+          })}
+        </div>
+      </div>
+      <div className='divider'></div>
+      <div className='rocks-table'>
+        {allRocks.map((rock) => {
+          return <div className={pickedRocks.includes(rock) ? 'active' : ""}>
+            <span key={rock}>{rock}</span>
+          </div>
+        })}
+      </div>
     </div>
   );
 }
